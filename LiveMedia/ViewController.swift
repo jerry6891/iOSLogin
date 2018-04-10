9e5b3691b2032d5727b13a35abaee1788ddf2fac
jerrycastillo <gweb1986@gmail.com>
2018-04-09 17:03:29 -0700

    Initial Commit

---
+++ LiveMedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveMedia
+//
+//  Created by [T.T.S.D.] on 2018-04-09.
+//  Copyright © 2018 GWEB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
